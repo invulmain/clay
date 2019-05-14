651f0ddb0a953c586dc0dbf778b12e8653a88579
invulmain <39603833+invulmain@users.noreply.github.com>
2019-05-14 18:06:17 +0700

    2

--- sravnenie.sh
+++ sravnenie.sh
@@ -6,7 +6,11 @@ readarray -t mass < <(ls /home/user/clay/12.0)
 #echo ${#mass[@]}
 
 for i in "${mass[@]}"; do
-	echo sravnenue: + "$i";
+	echo sravnenue: "$i"
+	tek="diff <(xxd /hive/miners/claymore/12.0/$i) <(xxd /home/user/clay/12.0/$i) > /home/user/clay/result"
+	cat tek
+	eval $tek
+	if [[ -s /home/user/clay/result ]]; then echo "$i: raznie"; else echo "$i: OK"; fi
 
 done
 
