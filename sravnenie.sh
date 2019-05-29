9443f7f76cd0bb0fb7907efdfaada3e7f26f0c1d
invulmain <39603833+invulmain@users.noreply.github.com>
2019-05-29 10:05:10 +0700

    1

--- sravnenie.sh
+++ sravnenie.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-readarray -t mass < <(ls /home/user/clay/12.0)
+readarray -t mass < <(ls /home/user/clay/14.6)
 
 #kolvo
 #echo ${#mass[@]}
 
-adresminer="/hive/miners/claymore/12.0/"
+adresminer="/hive/miners/claymore/14.6/"
 
 for i in "${mass[@]}"; do
 	if ! test -f "$adresminer$i"; then
@@ -13,7 +13,7 @@ for i in "${mass[@]}"; do
 		continue
 	fi
 	#echo sravnenue: "$i"
-	tek="diff <(xxd $adresminer$i) <(xxd /home/user/clay/12.0/$i) > /home/user/clay/result"
+	tek="diff <(xxd $adresminer$i) <(xxd /home/user/clay/14.6/$i) > /home/user/clay/result"
 	#echo $tek
 	eval $tek
 	if [[ -s /home/user/clay/result ]]; then echo "$i	raznie"; else echo "$i	OK"; fi
